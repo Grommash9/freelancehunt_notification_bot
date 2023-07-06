8b9c5c94c6794c0bbe3352d5f293313aaf839e22
Oleksandr Prudnikov <oleksandr.prudnikov@cakealliance.io>
2023-07-06 12:24:59 +0100

    new main and misc files

--- freelancehunt_bot/misc.py
+++ freelancehunt_bot/misc.py
@@ -5,11 +5,8 @@ import os
 from aiogram import Dispatcher, Bot
 from aiogram import types
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
-from aiogram.contrib.middlewares.i18n import I18nMiddleware
 from aiohttp import web
-from babel.core import Locale
-from bot_app.config import BOT_TOKEN, REDIS
-from bot_app.config import I18N_DOMAIN, LOCALES_DIR
+from freelancehunt_bot.config import BOT_TOKEN, REDIS
 
 loop = get_event_loop()
 bot = Bot(BOT_TOKEN, parse_mode='HTML')
